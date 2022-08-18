fdc81ab63d902e8888737932ccc582507c75eebd
Apple <apple@Apples-MacBook-Pro.local>
2022-08-18 10:47:45 +0530

    Added scriptable object for character 1

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MakeOver_SO", menuName = "ScriptableObject/MakeOver/Character_SO")]
+public class Character : ScriptableObject
+{
+    public GameObject GirlModel;
+    //public List<MaterialsProperties> materialsProperties;
+    //public List<TextureProperties> textureProperties;
+    public CharacterSOData modelProperties;
+}
+
+[System.Serializable]
+public class MaterialsProperties
+{
+    public Material ModelPartMaterial;
+    public Texture2D Thumbnail;
+    public ModelPropertyType type;
+}
+
+[System.Serializable]
+public class TextureProperties
+{
+    public Texture2D Texture;
+    public Texture2D Thumbnail;
+    public ModelPropertyType type;
+}
+
+[System.Serializable]
+public class ModelProperties
+{
+    public GameObject modelObject;
+    public Texture2D modelThumbnails;
+    public Material ModelPartMaterial;
+    public string modelName;
+}
+
+[System.Serializable]
+public class Category
+{
+    public ModelPropertyType type;
+    public ModelProperties modelProperties;
+}
+
+[System.Serializable]
+public class CharacterSOData
+{
+    public Transform modelPositionTransform;
+    public Texture2D CharacterThumbnail;
+    //public Vector3 modelInstantiationPosition;
+    public List<Category> categories;
+}
+
+
+public enum ModelPropertyType
+{
+    Hair, Body, FaceStyle, Shoes, Jeans, 
+}
---
+++ Assets/Scripts/CharacterModelController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterModelController : MonoBehaviour
+{
+    [SerializeField] List<Character> _characterScriptableObject; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
